a7762d43eee80e3dc38b6cb2190956e2e08cfe23
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2024-04-01 13:58:02 +0400

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/navbar"
 
 function App() {
 
   return (
-    <h1 className="text-10xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+    </>
   )
 }
 
