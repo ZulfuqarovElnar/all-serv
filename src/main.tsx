28a15f61437479db04d0386ef45124250b2665d3
Elnar Zulfuqarli <elnarzulfuqarli2001@gmail.com>
2024-04-13 21:54:41 +0400

    Fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
 
-import "@fontsource/pacifico";
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
